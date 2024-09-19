fc933ec14fee978daa7d4344085201492a67e88a
Jeremy Ipa <jeremy.iparraguirre@gmail.com>
2024-09-19 13:20:18 +0200

    fix

--- app/pricing/components/Pricing.tsx
+++ app/pricing/components/Pricing.tsx
@@ -7,18 +7,16 @@ const Pricing: React.FC = () => {
    return (
       <div className="container mx-auto pt-24">
          <h1 className="text-3xl font-bold text-center mb-4 md:text-2xl sm:text-4xl text-black">
-            Nos Tarifs
-         </h1>
-         <h2 className="text-black">
-            Découvrez nos différentes{" "}
-            <span className="font-bold border-b-2 border-black text-green-300">offres</span>{" "}
+         Découvrez nos différentes{" "}
+            <span className="font-bold border-b-2 border-green-300 text-green-300">offres</span>{" "}
             adaptées à vos{" "}
-            <span className="font-bold border-b-2 border-orange-300">
+            <span className="font-bold border-b-2 text-orange-300 border-orange-300">
                besoins
             </span>{" "}
             .
-         </h2>
-         <p className="text-black m-4 text-2xl font-semibold ">Création de sites web</p>
+         </h1>
+        
+         <p className="text-black m-4 text-3xl font-semibold text-center">- Création de sites web -</p>
          <article>
             <Web />
          </article>
--- app/pricing/components/ui/web.tsx
+++ app/pricing/components/ui/web.tsx
@@ -1,53 +1,89 @@
-import React from 'react'
+import React from "react";
 
 function Web() {
-  return (
-    <div>
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="border rounded-lg p-6 shadow-lg">
-            <h2 className="text-2xl font-semibold mb-2">Starter Pack</h2>
-            <p className="text-xl mb-4">€19/mois</p>
-            <ul className="mb-4">
-              <li>Fonctionnalité 1</li>
-              <li>Fonctionnalité 2</li>
-              <li>Support de base</li>
-            </ul>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-              Choisir
-            </button>
-          </div>
-          
-          <div className="border rounded-lg p-6 shadow-lg">
-            <h2 className="text-2xl font-semibold mb-2">Standard</h2>
-            <p className="text-xl mb-4">€49/mois</p>
-            <ul className="mb-4">
-              <li>Fonctionnalité 1</li>
-              <li>Fonctionnalité 2</li>
-              <li>Fonctionnalité 3</li>
-              <li>Support premium</li>
-            </ul>
-            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-              Choisir
-            </button>
-          </div>
-          
-          <div className="border rounded-lg p-6 shadow-lg">
-            <h2 className="text-2xl font-semibold mb-2">Premium</h2>
-            <p className="text-xl mb-4">€99/mois</p>
-            <ul className="mb-4">
-              <li>Fonctionnalité 1</li>
-              <li>Fonctionnalité 2</li>
-              <li>Fonctionnalité 3</li>
-              <li>Fonctionnalité 4</li>
-              <li>Support 24/7</li>
-            </ul>
-            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Choisir
-            </button>
-          </div>
-        </div>
-    </div>
-  )
+   return (
+      <div>
+         <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="border rounded-lg p-6 shadow-lg">
+               <h2 className="text-2xl font-semibold mb-2 text-black text-center">
+                  Starter Pack
+               </h2>
+               <p className="text-xl mb-4 text-black font-semibold">999€</p>
+               <p className="text-black pb-4">
+                  Adapté aux petites entreprises ainsi qu'aux start-up
+               </p>
+               <div className="w-full  border-t border-gray-500 my-4"></div>
+               <ul className="mb-4 text-black list-disc list-inside py-4">
+                  <li>Nom de domaine</li>
+                  <li>Hébergement web</li>
+                  <li>Site vitrine</li>
+                  <li>Page d'accueil</li>
+                  <li>Formulaire de contact</li>
+               </ul>
+               <a
+                  href="https://cal.com/j-idev/30min?user=j-idev"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-300 text-black  rounded hover:bg-green-600 relative w-full sm:w-auto px-4 py-2 sm:px-6 sm:py-3 font-semibold text-sm sm:text-lg shadow-lg "
+               >
+                  Rendez-vous
+               </a>
+            </div>
+
+            <div className="border rounded-lg p-6 shadow-lg">
+               <h2 className="text-2xl font-semibold mb-2 text-black text-center">
+                  Premium Pack
+               </h2>
+               <p className="text-xl mb-4 text-black font-semibold">1499€</p>
+               <p className="text-black pb-4">
+                  Idéal pour les entreprises en croissance
+               </p>
+               <div className="w-full  border-t border-gray-500 my-4"></div>
+               <ul className="mb-4 text-black list-disc list-inside py-4">
+                  <li>Starter Pack (inclus)</li>
+                  <li>Prise de rendez-vous en ligne</li>
+                  <li>Boite mail professionelle</li>
+                  <li>3 Mois de maintenance offerte</li>
+                  
+               </ul>
+               <a
+                  href="https://cal.com/j-idev/30min?user=j-idev"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-300 text-black  rounded hover:bg-green-600 relative w-full sm:w-auto px-4 py-2 sm:px-6 sm:py-3 font-semibold text-sm sm:text-lg shadow-lg "
+               >
+                  Rendez-vous
+               </a>
+            </div>
+
+            <div className="border rounded-lg p-6 shadow-lg">
+               <h2 className="text-2xl font-semibold mb-2 text-black text-center">
+                  Custom Pack
+               </h2>
+               <p className="text-xl mb-4 text-black font-semibold">--- €</p>
+               <p className="text-black pb-4">
+                  Prix suivant devis, entièrement personnalisable
+               </p>
+               <div className="w-full  border-t border-gray-500 my-4"></div>
+               <ul className="mb-4 text-black list-disc list-inside py-4">
+                  <li>Premium Pack (inclus)</li>
+                  <li>Design sur mesure</li>
+                  <li>Solutions personnalisable sur devis</li>
+                  
+               </ul>
+               <a
+                  href="https://cal.com/j-idev/30min?user=j-idev"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-300 text-black  rounded hover:bg-green-600 relative w-full sm:w-auto px-4 py-2 sm:px-6 sm:py-3 font-semibold text-sm sm:text-lg shadow-lg "
+               >
+                  Rendez-vous
+               </a>
+            </div>
+
+         </div>
+      </div>
+   );
 }
 
-export default Web
+export default Web;
